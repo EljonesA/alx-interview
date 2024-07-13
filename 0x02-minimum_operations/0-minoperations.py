23c3310668e58b6f4d5050ef19b994eb753fe602
Eljones <eljones.odongo@gmail.com>
2024-07-13 08:13:24 +0300

    Minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+def minOperations(n):
+    '''
+    args:
+        - n: number of characters required
+    '''
+    if n == 1:
+        return 0
+
+    dp = {1: 0}  # Base case: 1 H requires 0 operations
+    for length in range(2, n + 1):
+        min_ops = float('inf')
+        for j in range(1, length // 2 + 1):
+            if length % j == 0:
+                min_ops = min(min_ops, dp[j] + length // j)
+        dp[length] = min_ops
+
+    return dp.get(n, 0)
